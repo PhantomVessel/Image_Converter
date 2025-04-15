656be522278d8a5d6134b8596d7c9531b8098986
PhantomVessel <jonathan.huettinger@proton.me>
2025-04-16 00:58:03 +0200

    Converter as mvp

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from PIL import Image
-import PIL
-print(PIL.__version__)
 
+img = Image.open("Images/black.png")
+
+
+
+img.save("your_icon.ico", format='ICO', sizes=[(256, 256), (128, 128), (64, 64), (32, 32), (16, 16)])
+
+print("✅ Converted to ICO successfully!")
 
